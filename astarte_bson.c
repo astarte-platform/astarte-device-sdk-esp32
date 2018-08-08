8861ce0b0720b6d0c279b353bfc3d1560ee0a9c6
Davide Bettio <davide.bettio@ispirata.com>
2018-08-08 18:41:16 +0200

    Add BSON reader
    Add BSON reader functions based on alrady existing SDKs code.

---
+++ astarte_bson.c
@@ -0,0 +1,270 @@
+/*
+ * (C) Copyright 2016, Ispirata Srl, info@ispirata.com.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+ OR Apache-2.0
+ */
+
+#include <astarte_bson.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <endian.h>
+
+#include <esp_log.h>
+
+#define TYPE_DOUBLE 0x01
+#define TYPE_STRING 0x02
+#define TYPE_DOCUMENT 0x03
+#define TYPE_BINARY 0x05
+#define TYPE_BOOLEAN 0x08
+#define TYPE_DATETIME 0x09
+#define TYPE_INT32 0x10
+#define TYPE_INT64 0x12
+
+#define TAG "ASTARTE_BSON"
+
+static uint32_t read_uint32(const void *u)
+{
+    const unsigned char *b = (const unsigned char *) u;
+    return le32toh(((uint32_t) b[0]) | (((uint32_t) b[1]) << 8) | (((uint32_t) b[2]) << 16) | (((uint32_t) b[3]) << 24));
+}
+
+static uint64_t read_uint64(const void *u)
+{
+    const unsigned char *b = (const unsigned char *) u;
+    return le64toh((uint64_t) b[0] | ((uint64_t) b[1] << 8) | ((uint64_t) b[2] << 16) | ((uint64_t) b[3] << 24) | ((uint64_t) b[4] << 32) | ((uint64_t) b[5] << 40) | ((uint64_t) b[6] << 48) | ((uint64_t) b[7] << 56));
+}
+
+static unsigned int astarte_bson_next_item_offset(unsigned int offset, unsigned int keyLen, const void *document)
+{
+    const char *docBytes = (const char *) document;
+    uint8_t elementType = (uint8_t) docBytes[offset];
+
+    /* offset <- type (uint8_t) + key (const char *) + '\0' (char) */
+    offset += 1 + keyLen + 1;
+
+    switch (elementType) {
+        case TYPE_STRING: {
+            uint32_t stringLen = read_uint32(docBytes + offset);
+            offset += stringLen + 4;
+        }
+        break;
+
+        case TYPE_DOCUMENT: {
+            uint32_t docLen = read_uint32(docBytes + offset);
+            offset += docLen;
+        }
+        break;
+
+        case TYPE_BINARY: {
+            uint32_t binLen = read_uint32(docBytes + offset);
+            offset += 4 + 1 + binLen; /* int32 (len) + byte (subtype) + binLen */
+        }
+        break;
+
+        case TYPE_INT32: {
+           offset += sizeof(int32_t);
+        }
+        break;
+
+        case TYPE_DOUBLE:
+        case TYPE_DATETIME:
+        case TYPE_INT64: {
+            offset += sizeof(int64_t);
+        }
+        break;
+
+        case TYPE_BOOLEAN: {
+            offset += 1;
+        }
+        break;
+
+        default: {
+            ESP_LOGW(TAG, "unrecognized BSON type: %i", (int) elementType);
+            return 0;
+        }
+    }
+
+    return offset;
+}
+
+const void *astarte_bson_key_lookup(const char *key, const void *document, uint8_t *type)
+{
+    const char *docBytes = (const char *) document;
+    uint32_t docLen = read_uint32(document);
+
+    /* TODO: it would be nice to check len validity here */
+
+    unsigned int offset = 4;
+    while (offset + 1 < docLen) {
+       uint8_t elementType = (uint8_t) docBytes[offset];
+       int keyLen = strnlen(docBytes + offset + 1, docLen - offset);
+
+       if (!strncmp(key, docBytes + offset + 1, docLen - offset)) {
+           if (type) {
+               *type = elementType;
+           }
+           return (void *) (docBytes + offset + 1 + keyLen + 1);
+       }
+
+       unsigned int newOffset = astarte_bson_next_item_offset(offset, keyLen, document);
+       if (!newOffset) {
+           return NULL;
+       }
+       offset = newOffset;
+    }
+
+    return NULL;
+}
+
+void *astarte_bson_next_item(const void *document, const void *current_item)
+{
+    const char *docBytes = (const char *) document;
+    uint32_t docLen = read_uint32(document);
+    unsigned int offset = ((const char *) current_item) - docBytes;
+
+    if (offset + 1 >= docLen) {
+        return NULL;
+    }
+
+    int keyLen = strnlen(docBytes + offset + 1, docLen - offset);
+    unsigned int newOffset = astarte_bson_next_item_offset(offset, keyLen, document);
+
+    if (!newOffset) {
+        return NULL;
+    }
+
+    if (newOffset + 1 >= docLen) {
+        return NULL;
+    }
+
+    return ((char *) docBytes) + newOffset;
+}
+
+const void *astarte_bson_first_item(const void *document)
+{
+    const char *docBytes = (const char *) document;
+    return docBytes + 4;
+}
+
+const char *astarte_bson_key(const void *item)
+{
+    return ((const char *) item) + 1;
+}
+
+const char *astarte_bson_value_to_string(const void *valuePtr, uint32_t *len)
+{
+    const char *valueBytes = (const char *) valuePtr;
+    uint32_t stringLen = read_uint32(valueBytes);
+
+    if (len) {
+        *len = stringLen - 1;
+    }
+
+    return valueBytes + 4;
+}
+
+const char *astarte_bson_value_to_binary(const void *valuePtr, uint32_t *len)
+{
+    const char *valueBytes = (const char *) valuePtr;
+    uint32_t binLen = read_uint32(valueBytes);
+
+    if (len) {
+        *len = binLen;
+    }
+
+    return valueBytes + 5;
+}
+
+const void *astarte_bson_value_to_document(const void *valuePtr, uint32_t *len)
+{
+    const char *valueBytes = (const char *) valuePtr;
+    uint32_t binLen = read_uint32(valueBytes);
+
+    if (len) {
+        *len = binLen;
+    }
+
+    return valueBytes;
+}
+
+int8_t astarte_bson_value_to_int8(const void *valuePtr)
+{
+    return ((int8_t *) valuePtr)[0];
+}
+
+int32_t astarte_bson_value_to_int32(const void *valuePtr)
+{
+    return (int32_t) read_uint32(valuePtr);
+}
+
+int64_t astarte_bson_value_to_int64(const void *valuePtr)
+{
+    return (int64_t) read_uint64(valuePtr);
+}
+
+double astarte_bson_value_to_double(const void *valuePtr)
+{
+    union data64 {
+        uint64_t u64value;
+        double dvalue;
+    } v;
+    v.u64value = read_uint64(valuePtr);
+    return v.dvalue;
+}
+
+int astarte_bson_check_validity(const void *document, unsigned int fileSize)
+{
+    const char *docBytes = (const char *) document;
+    uint32_t docLen = read_uint32(document);
+    int offset;
+
+    if (!fileSize) {
+        ESP_LOGW(TAG, "Empty buffer: no BSON document found");
+        return 0;
+    }
+
+    if ((docLen == 5) && (fileSize >= 5) && (docBytes[4] == 0)) {
+        // empty document
+        return 1;
+    }
+
+    if (fileSize < 4 + 1 + 2 + 1) {
+        ESP_LOGW(TAG, "BSON data too small");
+        return 0;
+    }
+
+    if (docLen > fileSize) {
+        ESP_LOGW(TAG, "BSON document is bigger than data: data: %ui document: %i", fileSize, (int) docLen);
+        return 0;
+    }
+
+    if (docBytes[docLen - 1] != 0) {
+        ESP_LOGW(TAG, "BSON document is not terminated by null byte.");
+        return 0;
+    }
+
+    offset = 4;
+    switch (docBytes[offset]) {
+       case TYPE_DOUBLE:
+       case TYPE_STRING:
+       case TYPE_DOCUMENT:
+       case TYPE_BINARY:
+       case TYPE_BOOLEAN:
+       case TYPE_DATETIME:
+       case TYPE_INT32:
+       case TYPE_INT64:
+       break;
+
+       default:
+           ESP_LOGW(TAG, "Unrecognized BSON document first type\n");
+           return 0;
+    }
+
+    return 1;
+}
+
+int32_t astarte_bson_document_size(const void *document)
+{
+    return read_uint32(document);
+}
