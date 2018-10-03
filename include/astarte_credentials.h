1a6f125ed034c7c98d4ce6714e6b4971c11c3706
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-10-03 16:14:50 +0200

    astarte_credentials: add astarte_credentials_get_certificate_common_name
    Needed to know the device topic

--- include/astarte_credentials.h
+++ include/astarte_credentials.h
@@ -78,6 +78,16 @@ astarte_err_t astarte_credentials_get_csr(char *out, size_t length);
 astarte_err_t astarte_credentials_get_certificate(char *out, size_t length);
 
 /**
+ * @brief get the certificate Common Name
+ *
+ * @details Get the certificate Common Name, writing it to the out buffer, if it is present.
+ * @param out A pointer to an allocated buffer where the CN will be written.
+ * @param length The length of the out buffer.
+ * @return The status code, ASTARTE_OK if the certificate was correctly written, otherwise an error code is returned.
+ */
+astarte_err_t astarte_credentials_get_certificate_common_name(char *out, size_t length);
+
+/**
  * @brief get the private key to connect with the Astarte MQTT v1 protocol
  *
  * @details Get the private key, writing it to the out buffer, if it is present.
