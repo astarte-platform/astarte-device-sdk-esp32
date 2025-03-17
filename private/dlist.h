3d71121c9775a209ce1dac5196218fdb442dd379
Simone Orru <simone.orru@secomind.com>
2025-03-17 09:35:29 +0100

    Port for the introspection
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

---
+++ private/dlist.h
@@ -0,0 +1,133 @@
+/*
+ * (C) Copyright 2023-2025, SECO Mind Srl
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file dlist.h
+ * @brief Utility module containing a doubly linked list implementation.
+ *
+ * @details This library does not perform deep copies when storing values.
+ * The user should ensure that any value is freed correctly when appropriate.
+ */
+
+#ifndef DLIST_H
+#define DLIST_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "astarte_device_sdk/astarte.h"
+#include "astarte_device_sdk/result.h"
+
+typedef struct
+{
+    struct dlist_node *head;
+    struct dlist_node *tail;
+} dlist_t;
+
+typedef struct
+{
+    dlist_t *handle;
+    struct dlist_node *node;
+} dlist_iterator_t;
+
+/**
+ * @brief Initializes a new empty linked list
+ *
+ * @return A handle to the newly initialized list
+ */
+dlist_t dlist_init(void);
+
+/**
+ * @brief Checks if the linked list is empty
+ *
+ * @param[in] handle Linked list handle
+ * @return true if the list is empty, false otherwise
+ */
+bool dlist_is_empty(dlist_t *handle);
+
+/**
+ * @brief Appends an item to the end of a linked list
+ *
+ * @param[inout] handle Linked list handle
+ * @param[in] value Item to append to the linked list
+ * @return One of the follwing error codes:
+ * @retval ASTARTE_ERR_OUT_OF_MEMORY if memory allocation failed
+ * @retval ASTARTE_RESULT_OK if operation has been successful
+ */
+astarte_result_t dlist_append(dlist_t *handle, void *value);
+
+/**
+ * @brief Removes and returns the last item from a linked list
+ *
+ * @param[inout] handle Linked list handle
+ * @param[out] value Item removed from the list
+ * @return NULL when the list was empty an no item has been removed, the removed item otherwise
+ */
+void *dlist_remove_tail(dlist_t *handle);
+
+/**
+ * @brief Destroys the list without de-allocating its content
+ *
+ * @note Must be called on an non-empty list when its use has ended. While it releases all the
+ * internal structures of the list it does not free the content of each item.
+ *
+ * @param[inout] handle Linked list handle
+ */
+void dlist_destroy(dlist_t *handle);
+
+/**
+ * @brief Destroys the list releasing with 'free()' its content
+ *
+ * @note Can be called on an non-empty list when its use has ended. It releases all the internal
+ * structures of the list as well as each item placed into the list.
+ *
+ * @param[inout] handle Linked list handle
+ */
+void dlist_destroy_and_release(dlist_t *handle);
+
+/**
+ * @brief Initializes an iterator over a linked list
+ *
+ * @note After intialization the iterator will be pointing to the first item of the list.
+ *
+ * @param[in] handle Linked list handle
+ * @param[out] iterator Iterator to initialize
+ * @return One of the follwing error codes:
+ * @retval ASTARTE_RESULT_NOT_FOUND if list is empty
+ * @retval ASTARTE_RESULT_OK if operation has been successful
+ */
+astarte_result_t dlist_iterator_init(dlist_t *handle, dlist_iterator_t *iterator);
+
+/**
+ * @brief Advances the iterator to the next item of the list
+ *
+ * @param[inout] iterator Iterator to advance
+ * @return One of the follwing error codes:
+ * @retval ASTARTE_RESULT_NOT_FOUND if the end of the list has been reached
+ * @retval ASTARTE_RESULT_OK if operation has been successful
+ */
+astarte_result_t dlist_iterator_advance(dlist_iterator_t *iterator);
+
+/**
+ * @brief Gets the item pointed by the iterator
+ *
+ * @param[in] iterator Iterator to use for the operation
+ * @return The list item pointed by the iterator
+ */
+void *dlist_iterator_get_item(dlist_iterator_t *iterator);
+
+/**
+ * @brief Replaces the item pointed by the iterator
+ *
+ * @note Does not de-allocate the old item content.
+ *
+ * @param[inout] iterator Iterator to use for the operation
+ * @param[in] value New element to insert in the linked list
+ * @return The item that has been replaced
+ */
+void *dlist_iterator_replace_item(dlist_iterator_t *iterator, void *value);
+
+#endif // DLIST_H
---
+++ src/dlist.c
@@ -0,0 +1,152 @@
+/*
+ * (C) Copyright 2023-2025, SECO Mind Srl
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR Apache-2.0
+ */
+
+#include <dlist.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_log.h>
+
+#include "astarte_device_sdk/result.h"
+
+/************************************************
+ *        Defines, constants and typedef        *
+ ***********************************************/
+
+#define TAG "ASTARTE_DLIST"
+
+struct dlist_node
+{
+    struct dlist_node *next;
+    struct dlist_node *prev;
+    void *value;
+};
+
+/************************************************
+ *         Global functions definitions         *
+ ***********************************************/
+
+dlist_t dlist_init(void)
+{
+    dlist_t handle = { .head = NULL, .tail = NULL };
+    return handle;
+}
+
+bool dlist_is_empty(dlist_t *handle)
+{
+    return (handle->head == NULL) && (handle->tail == NULL);
+}
+
+astarte_result_t dlist_append(dlist_t *handle, void *value)
+{
+    // Allocate a new node for the struct
+    struct dlist_node *node = calloc(1, sizeof(struct dlist_node));
+    if (!node) {
+        ESP_LOGE(TAG, "Out of memory %s: %d", __FILE__, __LINE__);
+        return ASTARTE_RESULT_OUT_OF_MEMORY;
+    }
+    node->value = value;
+
+    // If list is empty, add first node. Otherwise, add node at the end of list.
+    node->next = NULL;
+    if (dlist_is_empty(handle)) {
+        node->prev = NULL;
+        handle->head = node;
+    } else {
+        node->prev = handle->tail;
+        handle->tail->next = node;
+    }
+    handle->tail = node;
+    return ASTARTE_RESULT_OK;
+}
+
+void *dlist_remove_tail(dlist_t *handle)
+{
+    if (dlist_is_empty(handle)) {
+        return NULL;
+    }
+
+    struct dlist_node *last_node = handle->tail;
+    // Check if list contains a single node or multiple nodes
+    if (handle->head == handle->tail) {
+        handle->head = NULL;
+        handle->tail = NULL;
+    } else {
+        last_node->prev->next = NULL;
+        handle->tail = last_node->prev;
+    }
+    void *value = last_node->value;
+    free(last_node);
+    return value;
+}
+
+void dlist_destroy(dlist_t *handle)
+{
+    if (!dlist_is_empty(handle)) {
+        struct dlist_node *node = handle->head;
+        struct dlist_node *next_node = node->next;
+        while (next_node) {
+            free(node);
+            node = next_node;
+            next_node = node->next;
+        }
+        // Free the last node
+        free(node);
+        handle->head = NULL;
+        handle->tail = NULL;
+    }
+}
+
+void dlist_destroy_and_release(dlist_t *handle)
+{
+    if (!dlist_is_empty(handle)) {
+        struct dlist_node *node = handle->head;
+        struct dlist_node *next_node = node->next;
+        while (next_node) {
+            free(node->value);
+            free(node);
+            node = next_node;
+            next_node = node->next;
+        }
+        // Free the last node
+        free(node->value);
+        free(node);
+        handle->head = NULL;
+        handle->tail = NULL;
+    }
+}
+
+astarte_result_t dlist_iterator_init(dlist_t *handle, dlist_iterator_t *iterator)
+{
+    if (dlist_is_empty(handle)) {
+        return ASTARTE_RESULT_NOT_FOUND;
+    }
+    iterator->handle = handle;
+    iterator->node = handle->head;
+    return ASTARTE_RESULT_OK;
+}
+
+astarte_result_t dlist_iterator_advance(dlist_iterator_t *iterator)
+{
+    if (iterator->node == iterator->handle->tail) {
+        return ASTARTE_RESULT_NOT_FOUND;
+    }
+    iterator->node = iterator->node->next;
+    return ASTARTE_RESULT_OK;
+}
+
+void *dlist_iterator_get_item(dlist_iterator_t *iterator)
+{
+    return iterator->node->value;
+}
+
+void *dlist_iterator_replace_item(dlist_iterator_t *iterator, void *value)
+{
+    void *old = iterator->node->value;
+    iterator->node->value = value;
+    return old;
+}
