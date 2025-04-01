db3ad352c97e6e4cbcebf714b3380bc6b7b71197
Simone Orru <simone.orru@secomind.com>
2025-04-01 15:45:01 +0200

    Fix tidy in CI
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- tidy.sh
+++ tidy.sh
@@ -18,7 +18,7 @@ display_help() {
 
 # Set defaults for the command line arguments
 fresh_mode=false
-esp_path=$HOME/esp
+esp_path=$HOME/esp/esp-idf
 sample=astarte_app
 file=""
 
@@ -45,21 +45,21 @@ cd ./samples/"$sample" || exit 1
 export IDF_TOOLCHAIN="clang"
 
 # Export idf.py
-if [ ! -f "$esp_path/esp-idf/export.sh" ]; then
-    echo "Could not find the ESP IDF export script: '$esp_path/esp-idf/export.sh'"
+if [ ! -f "$esp_path/export.sh" ]; then
+    echo "Could not find the ESP IDF export script: '$esp_path/export.sh'"
     exit 1
 fi
-. "$esp_path"/esp-idf/export.sh || exit 1
+. "$esp_path"/export.sh || exit 1
 
 # Install dependencies if required
 idf_tools.py install esp-clang
 
 # Re-export idf.py
-if [ ! -f "$esp_path/esp-idf/export.sh" ]; then
-    echo "Could not find the ESP IDF export script: '$esp_path/esp-idf/export.sh'"
+if [ ! -f "$esp_path/export.sh" ]; then
+    echo "Could not find the ESP IDF export script: '$esp_path/export.sh'"
     exit 1
 fi
-. "$esp_path"/esp-idf/export.sh
+. "$esp_path"/export.sh
 
 # Re-generate the compilation database if fresh is selected
 if $fresh_mode; then
@@ -76,11 +76,10 @@ if [ ! -d ".venv" ]; then
 fi
 source .venv/bin/activate
 package_name="colored"
-package_version="2.3.0"
 installed_version=$( (pip show $package_name | grep Version | awk '{print $2}') || true)
-if [ "$installed_version" != "$package_version" ]; then
-    if ! pip install $package_name==$package_version; then
-        echo "Failed to install $package_name version $package_version."
+if [ "$installed_version" == "" ]; then
+    if ! pip install $package_name; then
+        echo "Failed to install $package_name."
         exit 1
     fi
 fi
